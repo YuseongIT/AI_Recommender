de36c999edfef41917a4d817b23e8c0593025700
YuseongIT <147607553+YuseongIT@users.noreply.github.com>
2026-03-02 20:05:29 +0800

    Initial commit: Set up Django project with MongoDB and AI recommender

---
+++ store/models.py
@@ -0,0 +1,14 @@
+from djongo import models
+
+class Product(models.Model):
+    _id = models.ObjectIdField()
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.FloatField()
+    category = models.CharField(max_length=50)
+
+    # Hello! I added this code snippet for the challenge task.
+    tags = models.CharField(max_length=255, blank=True) 
+
+    def __str__(self):
+        return self.name
